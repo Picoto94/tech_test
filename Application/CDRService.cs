80ff9af3b8c9a617e557d3906ceee6c1813bed32
João Medeiros <joao.medeiros@medicineone.net>
2024-03-22 13:01:54 +0000

    Final commit

---
+++ Application/CDRService.cs
@@ -0,0 +1,75 @@
+﻿using Domain.Entities;
+using Infrastructure;
+
+namespace Application
+{
+    public class CDRService : ICDRService
+    {
+        private readonly ICDRRepository _cdrRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CDRService"/> class.
+        /// </summary>
+        /// <param name="cdrRepository">The CDR repository to be used for data access.</param>
+        public CDRService(ICDRRepository cdrRepository)
+        {
+            _cdrRepository = cdrRepository ?? throw new ArgumentNullException(nameof(cdrRepository));
+        }
+
+        /// <summary>
+        /// Retrieves a CDR by its reference asynchronously.
+        /// </summary>
+        /// <param name="reference">The unique reference of the CDR to retrieve.</param>
+        /// <returns>The CDR with the specified reference.</returns>
+        public async Task<CDR> GetByReferenceAsync(string reference)
+        {
+            return await _cdrRepository.GetByReferenceAsync(reference);
+        }
+
+        /// <summary>
+        /// Retrieves the count and total duration of all calls within a specified time period asynchronously.
+        /// </summary>
+        /// <param name="startDate">The start date of the time period.</param>
+        /// <param name="endDate">The end date of the time period.</param>
+        /// <returns>A dictionary containing the count and total duration of calls.</returns>
+        public async Task<Dictionary<string, object>> GetCallCountAndTotalDurationAsync(DateTime startDate, DateTime endDate)
+        {
+            var filteredCdrs = await _cdrRepository.GetCdrsByDateRangeAsync(startDate, endDate);
+
+            var count = filteredCdrs.Count();
+            var totalDuration = filteredCdrs.Sum(a => a.Duration);
+
+            return new Dictionary<string, object>
+            {
+                { "Count", count },
+                { "TotalDuration", totalDuration }
+            };
+        }
+
+        /// <summary>
+        /// Retrieves all CDRs for a specific caller ID within a specified time period asynchronously.
+        /// </summary>
+        /// <param name="callerId">The caller ID for which to retrieve CDRs.</param>
+        /// <param name="startDate">The start date of the time period.</param>
+        /// <param name="endDate">The end date of the time period.</param>
+        /// <returns>A collection of CDRs for the specified caller ID.</returns>
+        public async Task<IEnumerable<CDR>> GetCdrsByCallerIdAsync(string callerId, DateTime startDate, DateTime endDate)
+        {
+            return await _cdrRepository.GetCdrsByCallerIdAsync(callerId, startDate, endDate);
+        }
+
+        /// <summary>
+        /// Retrieves the N most expensive calls for a specific caller ID within a specified time period asynchronously.
+        /// </summary>
+        /// <param name="callerId">The caller ID for which to retrieve the most expensive calls.</param>
+        /// <param name="startDate">The start date of the time period.</param>
+        /// <param name="endDate">The end date of the time period.</param>
+        /// <param name="count">The number of most expensive calls to retrieve.</param>
+        /// <returns>A collection of the N most expensive calls for the specified caller ID.</returns>
+        public async Task<IEnumerable<CDR>> GetMostExpensiveCallsAsync(string callerId, DateTime startDate, DateTime endDate, int count)
+        {
+            var filteredCdrs = await _cdrRepository.GetCdrsByCallerIdAsync(callerId, startDate, endDate);
+            return filteredCdrs.OrderByDescending(a => a.Cost).Take(count);
+        }
+    }
+}
---
+++ Infrastructure/CDRRepository.cs
@@ -0,0 +1,181 @@
+﻿using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using System.Globalization;
+
+namespace Infrastructure
+{
+    /// <summary>
+    /// Repository for accessing Call Detail Records (CDRs) from CSV files.
+    /// </summary>
+    public class CDRRepository : ICDRRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IEnumerable<CDR> _cdrs;
+
+        /// <summary>
+        /// Initializes a new instance of the CDRRepository class.
+        /// </summary>
+        /// <param name="configuration">The configuration containing the path to the CSV files.</param>
+        public CDRRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _cdrs = ReadCDRsFromCsvDirectory();
+        }
+
+        /// <summary>
+        /// Retrieves all Call Detail Records (CDRs).
+        /// </summary>
+        /// <returns>An enumerable collection of CDRs.</returns>
+        public IEnumerable<CDR> GetAllCDRs()
+        {
+            return _cdrs;
+        }
+        /// <summary>
+        /// Reads CSV files in a specified directory.
+        /// </summary>
+        /// <returns>A list of CDRs read from all the CSV files.</returns>
+        private List<CDR> ReadCDRsFromCsvDirectory()
+        {
+            var csvDirectoryPath = Path.GetDirectoryName(Directory.GetCurrentDirectory()) + "\\Infrastructure";
+            var cdrs = new List<CDR>();
+
+            foreach (var csvFilePath in Directory.GetFiles(csvDirectoryPath, "*.csv"))
+            {
+                cdrs.AddRange(ReadCDRsFromCsv(csvFilePath));
+            }
+
+            return cdrs;
+        }
+
+        /// <summary>
+        /// Retrieves a Call Detail Record (CDR) by its reference asynchronously.
+        /// </summary>
+        /// <param name="reference">The reference of the CDR to retrieve.</param>
+        /// <returns>The CDR with the specified reference, or null if not found.</returns>
+        public async Task<CDR> GetByReferenceAsync(string reference)
+        {
+            return await Task.FromResult(_cdrs.FirstOrDefault(a => a.Reference == reference));
+        }
+
+        /// <summary>
+        /// Retrieves Call Detail Records (CDRs) for a specific caller ID within a specified date range asynchronously.
+        /// </summary>
+        /// <param name="callerId">The caller ID to filter the CDRs.</param>
+        /// <param name="startDate">The start date of the date range.</param>
+        /// <param name="endDate">The end date of the date range.</param>
+        /// <returns>An enumerable collection of CDRs filtered by the caller ID and date range.</returns>
+        public async Task<IEnumerable<CDR>> GetCdrsByCallerIdAsync(string callerId, DateTime startDate, DateTime endDate)
+        {
+            var filteredCdrs = FilterCdrsByDateRange(startDate, endDate)
+                                .Where(a => a.CallerId == callerId);
+            return await Task.FromResult(filteredCdrs);
+        }
+        /// <summary>
+        /// Reads CDRs from a CSV file
+        /// </summary>
+        /// <returns>A list of CDRs read from the CSV file.</returns>
+        private IEnumerable<CDR> ReadCDRsFromCsv(string csvFilePath)
+        {
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), csvFilePath);
+
+            var cdrs = new List<CDR>();
+
+            using (var reader = new StreamReader(fullPath))
+            {
+                // Ignore the header line
+                reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    var callerId = values[0];
+                    var recipient = values[1];
+                    var callDateStr = values[2].Trim();
+                    var endTimeStr = values[3].Trim();
+                    var durationStr = values[4].Trim();
+                    var costStr = values[5].Trim();
+                    var reference = values[6];
+                    var currency = values[7];
+                    var typeStr = values[8].Trim();
+
+                    DateTime callDate;
+                    DateTime endTime;
+                    double duration;
+                    decimal cost;
+                    CallType type;
+
+                    // Tenta converter as strings de data para DateTime
+                    if (!DateTime.TryParseExact(callDateStr, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out callDate))
+                    {
+                        callDate = DateTime.MinValue;
+                    }
+
+                    // Ajuste o formato da string de hora para incluir horas, minutos e segundos
+                    if (!DateTime.TryParseExact(endTimeStr, "HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out endTime))
+                    {
+                        endTime = DateTime.MinValue;
+                    }
+
+                    // Tenta converter a string de duração para um número
+                    if (!double.TryParse(durationStr, out duration))
+                    {
+                        duration = 0.0;
+                    }
+
+                    // Tenta converter a string de custo para um número
+                    if (!decimal.TryParse(costStr, out cost))
+                    {
+                        cost = 0;
+                    }
+
+                    // Tenta converter a string de tipo para o enum CallType
+                    if (!Enum.TryParse(typeStr, out type))
+                    {
+                        type = CallType.None;
+                    }
+
+                    var cdr = new CDR
+                    {
+                        CallerId = callerId,
+                        Recipient = recipient,
+                        CallDate = callDate,
+                        EndTime = endTime,
+                        Duration = duration,
+                        Cost = cost,
+                        Reference = reference,
+                        Currency = currency,
+                        Type = type
+                    };
+
+                    cdrs.Add(cdr);
+                }
+            }
+
+            return cdrs;
+        }
+        /// <summary>
+        /// Retrieves Call Detail Records (CDRs) for a specified date range.
+        /// </summary>
+        /// <param name="startDate">The start date of the date range.</param>
+        /// <param name="endDate">The end date of the date range.</param>
+        /// <returns>An enumerable collection of CDRs within the specified date range.</returns>
+        public async Task<IEnumerable<CDR>> GetCdrsByDateRangeAsync(DateTime startDate, DateTime endDate)
+        {
+            var filteredCdrs = await Task.FromResult(FilterCdrsByDateRange(startDate, endDate));
+
+            return filteredCdrs;
+        }
+        /// <summary>
+        /// Filters CDRs by date range.
+        /// </summary>
+        /// <param name="startDate">The start date of the date range.</param>
+        /// <param name="endDate">The end date of the date range.</param>
+        /// <returns>An enumerable collection of CDRs within the specified date range.</returns>
+        private IEnumerable<CDR> FilterCdrsByDateRange(DateTime startDate, DateTime endDate)
+        {
+            return _cdrs.Where(a => a.CallDate >= startDate && a.CallDate <= endDate);
+        }
+    }
+}
